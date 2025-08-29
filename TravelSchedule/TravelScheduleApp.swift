207b6ab529a2a66fc4fef02ad00cd8e7503d8b86
Medina <medinahuseynova@hotmail.com>
2025-08-29 12:55:55 +0400

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Medina Huseynova on 29.08.25.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelScheduleTests/TravelScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleTests.swift
+//  TravelScheduleTests
+//
+//  Created by Medina Huseynova on 29.08.25.
+//
+
+import Testing
+@testable import TravelSchedule
+
+struct TravelScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
